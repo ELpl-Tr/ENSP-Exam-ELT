772bd7a859c566ecc2ceec81b910c7e0d7267c9b
ELpl-Tr <emmanu65@gmail.com>
2025-02-24 09:22:57 +0000

    Début du nuage de point

--- ENSP-Exam-ELT/app.R
+++ ENSP-Exam-ELT/app.R
@@ -38,25 +38,23 @@ ui <- fluidPage(
                          "Afficher une notification")
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("nuage")
         )
     )
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
+    donnees <- reactive({
+      subset(diamonds, 
+             color == input$filtrer & 
+               price <= input$prix)
+    })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$nuage <- renderPlot({
+      ggplot(donnees(), aes(x = carat, y = price)) +
+        geom_point()
     })
 }
 
